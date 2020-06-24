395553760cd2fd0b52b91d8d462a8c3aff0329c4
Avinash <avi.khatwani@gmail.com>
2020-06-24 01:55:33 -0700

    Complete Cards

--- my-app/src/components/Cards/Cards.jsx
+++ my-app/src/components/Cards/Cards.jsx
@@ -7,6 +7,7 @@ import cx from 'classnames'
 const Cards = (props) => {
     // console.log(props)
     const daths = props.data.confirmed
+    const red = '#000'
     console.log(daths)
     if(!props.data.confirmed){
         return 'Loading...'
@@ -14,10 +15,10 @@ const Cards = (props) => {
     // console.log(props.data.confirmed.value)
     return(
         <div className={styles.container}>
-            <Grid container spacing={3} justify="center">
+            <Grid container spacing={4} justify="center">
                 <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                <CardContent className={styles.CardContent}>
+                        <Typography color="primary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
                             <CountUp start={0} end={props.data.confirmed.value} duration={3} separator=','/>
                         </Typography>
@@ -26,8 +27,8 @@ const Cards = (props) => {
                     </CardContent>
                 </Grid>
                 <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                    <CardContent className={styles.CardContent}>
+                        <Typography style={{ color: 'green' }} gutterBottom>Recovered</Typography>
                         <Typography variant="h5">
                             <CountUp start={0} end={props.data.recovered.value} duration={3} separator=','/>
                         </Typography>
@@ -36,8 +37,8 @@ const Cards = (props) => {
                     </CardContent>
                 </Grid>
                 <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                <CardContent className={styles.CardContent}>
+                        <Typography color="error" gutterBottom>Deaths</Typography>
                         <Typography variant="h5">
                             <CountUp start={0} end={props.data.deaths.value} duration={3} separator=','/>
                         </Typography>
